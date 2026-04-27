cb126b115d9f9ee9a27d7e1bdcdac363b7ebb8c3
smithevan-qanc <fwy25569@gmail.com>
2026-04-26 22:28:06 -0500

    [3.1.56] NOTES:

--- install-update.sh
+++ install-update.sh
@@ -11,7 +11,6 @@ install() {
     # Write Update Script Here
     echo "INSTALLING UPDATE"
     sudo systemctl restart ndpi-monitor-client.service
-    sudo lxpanelctl-pi notify "Software Update In Progress..."
     # sudo reboot
 }
 install
