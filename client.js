d764aef98eee339a2820b77306fd4ac6764bb6ff
smithevan-qanc <fwy25569@gmail.com>
2026-05-04 19:19:02 -0500

    [3.1.113] NOTES:

--- client.js
+++ client.js
@@ -747,7 +747,7 @@ class NDPiClient {
         });
     }
 
-    broadcastToDisplay(message) {
+    broadcastToDisplay(message = {}) {
         console.log(`( ⚡ ) Func: broadcastToDisplay`);
 
         console.log('1');
