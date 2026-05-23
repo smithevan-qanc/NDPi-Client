f99dcb3b442b8c5af2a025b75be242d5aff33718
smithevan-qanc <fwy25569@gmail.com>
2026-05-23 02:07:59 -0500

    [3.1.514] NOTES:

--- python/script/update_lcd.py
+++ python/script/update_lcd.py
@@ -30,7 +30,7 @@ try:
     disp.clear()
     disp.bl_DutyCycle(100)
     
-    Font1 = ImageFont.truetype("../Font/Consolas.ttf", 35)
+    Font1 = ImageFont.truetype("../Font/Consolas.ttf", 25)
     Font2 = ImageFont.truetype("../Font/Consolas.ttf", 30)
     Font3 = ImageFont.truetype("../Font/Consolas.ttf", 25)
     Font4 = ImageFont.truetype("../Font/Consolas.ttf", 20)
@@ -51,8 +51,8 @@ try:
         draw = ImageDraw.Draw(image1)
         
         # logging.info(f"Updating: {device_name} | {device_ip} | {ndi_status}")
-        draw.text((55, 17),     device_name,     fill="GREEN", font=Font1)
-        draw.text((25, 69),     "ID:",           fill="GRAY", font=Font4)
+        draw.text((20, 10),     device_name,     fill="GREEN", font=Font1)
+        draw.text((25, 35),     "ID:",           fill="GRAY", font=Font4)
         draw.text((55, 68),     device_id,       fill="GREEN", font=Font3)
         draw.text((25, 114),    "IP:",           fill="GRAY", font=Font4)
         draw.text((57, 110),    device_ip,       fill="GREEN", font=Font2)
