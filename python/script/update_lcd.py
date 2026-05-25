1417fccf653063906d6f0de448d7f37225e8d138
smithevan-qanc <fwy25569@gmail.com>
2026-05-25 12:54:13 -0500

    [3.1.589] NOTES:

--- python/script/update_lcd.py
+++ python/script/update_lcd.py
@@ -106,8 +106,8 @@ try:
 
         # CPU Temperature ------------------------------------------------------------
         sys_temp = f"{str(int(read_file('../../../../../sys/class/thermal/thermal_zone0/temp'))/1000)}°C"
-        sys_temp_x = get_centered_x(sys_temp, 20)
-        draw.text((sys_temp_x, 175), f"{sys_temp} | {time_string}", fill="GREEN", font=Consolas_Bold_20)
+        sys_temp_x = get_centered_x("##:## | ##.#°C", 20)
+        draw.text((sys_temp_x, 175), f"{time_string} | {sys_temp}", fill="GREEN", font=Consolas_Bold_20)
 
         # NDPi Version & Device IP ---------------------------------------------------
         line_dev_info = f"({ndpi_version})  {device_ip}".strip()
