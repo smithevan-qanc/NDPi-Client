2c043b257b6c3afa260d8e81bea479c6cca85254
smithevan-qanc <fwy25569@gmail.com>
2026-05-30 22:01:19 -0500

    [3.1.752] NOTES:

--- ndi_receiver_v3__NDI6/ndi_discover_v3.cpp
+++ ndi_receiver_v3__NDI6/ndi_discover_v3.cpp
@@ -109,7 +109,7 @@ void signal_handler(int signum) {
 }
 
 struct Options {
-    std::string version = "NDPi Discover (3.0.11)";
+    std::string version = "NDPi Discover (3.0.12)";
     std::string separator = "^";
     int timeout = 1;
 };
@@ -165,31 +165,32 @@ int main(int argc, char* argv[])
         else if (arg == "-h" || arg == "--help") {
             std::cout << "Usage: " << argv[0] << "[OPTIONS]\n\n";
             std::cout << "[OPTIONS]:\n";
-            std::cout << "\t" << "[-s|--single-line [<separator>]]" << "\t" << "Outputs one(1) source per line. E.g.:'NAME<seperator>URL'. (Default: '^')\n";
-            std::cout << "\t" << "[-t|--timeout [<seconds>]]" << "\t\t" << "How long to search for available sources before giving up. (Default: 1)\n";
-            std::cout << "\t" << "[-v|--version]" << "\t\t\t\t" << "NDPi Discover Version.\n";
-            std::cout << "\t" << "[-h|--help]" << "\t\t\t\t" << "This help menu." << std::endl;
+            std::cout << "    " << "[-s|--single-line [<separator>]]" << "\t" << "Outputs one(1) source per line. E.g.:'NAME<seperator>URL'. (Default: '^')\n";
+            std::cout << "    " << "[-t|--timeout [<seconds>]]" << "\t\t" << "How long to search for available sources before giving up. (Default: 1)\n";
+            std::cout << "    " << "                          " << "\t\t" << "NOTE: Increasing the '-timeout' will also increase the time it takes to shutdown.\n";
+            std::cout << "    " << "[-v|--version]" << "\t\t\t\t" << "NDPi Discover Version.\n";
+            std::cout << "    " << "[-h|--help]" << "\t\t\t\t" << "This help menu." << std::endl;
+
             return 0;
         } 
     }
 
-    // static auto last_discovery_time = std::chrono::steady_clock::now() - std::chrono::seconds(2);
-    // static bool output_pending = false;
+    static auto last_discovery_time = std::chrono::steady_clock::now() - std::chrono::seconds(5);
+    static bool output_pending = false;
 
     while (!g_shutdown) {
-        // auto current_time = std::chrono::steady_clock::now();
+
+        auto current_time = std::chrono::steady_clock::now();
         
         if (g_ndi.find_wait_for_sources(pNDI_find, timeout_seconds * 1000)) {
-        //     // New discovery detected - reset debounce timer
-        //     last_discovery_time = current_time;
-        //     output_pending = false;
-        // }
+            last_discovery_time = current_time;
+            output_pending = false;
+        }
 
-        // // Check if enough time has passed since last discovery
-        // auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(current_time - last_discovery_time).count();
+        auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(current_time - last_discovery_time).count();
 
-        // if (elapsed >= 1000 && !output_pending) {
-        //     output_pending = true;
+        if (elapsed >= 1000 && !output_pending) {
+            output_pending = true;
             
             uint32_t no_sources = 0;
             const NDIlib_source_t* p_sources = g_ndi.find_get_current_sources(pNDI_find, &no_sources);
