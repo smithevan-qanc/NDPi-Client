222b2c87a8f8167912320118d7a867679b3ab2ec
smithevan-qanc <fwy25569@gmail.com>
2026-06-16 18:38:16 -0500

    [3.1.1123] NOTES:

--- lcdDisplayUpdate.py
+++ lcdDisplayUpdate.py
@@ -41,9 +41,12 @@ def signal_handler(signum, frame):
 RST = 27
 DC = 25
 BL = 18
-bus = 0 
-device = 0 
+bus = 0
+device = 0
 # logging.basicConfig(level=logging.DEBUG)
+
+# Margin in pixels
+screen_margin = 10
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 font_dir = os.path.join(script_dir, "python", "Font")
@@ -55,15 +58,15 @@ def read_file(filepath):
     except:
         return "N/A"
     
-def get_centered_x(text, font_size, display_width=280):
+def get_centered_x(text, font_size, display_width=240):
     text_width = len(text) * (font_size * 0.55)
     x_coordinate = (display_width - text_width) / 2
-    return max(5, int(round(x_coordinate)))
+    return max(screen_margin, int(round(x_coordinate)))
 
-def get_right_x(text, font_size, margin=10, display_width=280):
+def get_right_x(text, font_size, margin=10, display_width=240):
     text_width = len(text) * (font_size * 0.55)
     x_coordinate = (display_width - text_width - margin)
-    return max(5, int(round(x_coordinate)))
+    return max(screen_margin, int(round(x_coordinate)))
     
 Consolas_Bold_30 = ImageFont.truetype(os.path.join(font_dir, "ConsolasBold.ttf"), 30)
 Consolas_Bold_25 = ImageFont.truetype(os.path.join(font_dir, "ConsolasBold.ttf"), 25)
@@ -95,10 +98,10 @@ try:
         
         # Read files
         device_name = read_file('python/script/device_name')
-        dev_nam_x = get_centered_x(device_name.strip(), 30)
+        dev_nam_x = screen_margin  # get_centered_x(device_name.strip(), 30)
 
         device_id = read_file('python/script/device_id')
-        dev_id_x = get_centered_x(device_id, 20)
+        dev_id_x = screen_margin  # get_centered_x(device_id, 20)
 
         device_ip = read_file('python/script/device_ip')
         ndpi_version = read_file('python/script/ndpi_version')
@@ -118,25 +121,25 @@ try:
             src_line_2 = ""
 
         # Draw on display ------------------------------------------------------------
-        image1 = Image.new("RGB", (disp.width, disp.height), "BLACK")
+        image1 = Image.new("RGB", (disp.width, disp.height), "#13265C")
         draw = ImageDraw.Draw(image1)
         
         # Device Name ----------------------------------------------------------------
-        draw.text((dev_nam_x, 15), device_name, fill="GREEN", font=font_roboto("Black", 25))
+        draw.text((dev_nam_x, screen_margin), device_name, fill="GRAY", font=font_roboto("Bold", 25))
 
         # Gray Line ------------------------------------------------------------------
         draw.line([(0, 52), (240, 52)], fill = "GRAY", width = 1)
 
         # NDI Status Label -----------------------------------------------------------
-        draw.text((10, 60), "NDI\nStatus", fill="GRAY", font=Consolas_Bold_20)
+        draw.text((10, 60), "NDI\nStatus", fill="GRAY", font=font_roboto("Medium", 25))
 
         # NDI Current State ----------------------------------------------------------
-        ndi_status_x = get_right_x(ndpi_status_ndi_status, 25)
-        draw.text((ndi_status_x, 60), ndpi_status_ndi_status, fill="GREEN", font=Consolas_25)
+        ndi_status_x = 40  # get_right_x(ndpi_status_ndi_status, 25)
+        draw.text((ndi_status_x, 60), ndpi_status_ndi_status, fill="GREEN", font=font_roboto("SemiBold", 25))
 
         # NDI Target Source ----------------------------------------------------------
-        src_x_1 = get_centered_x(src_line_1, 25)
-        src_x_2 = get_centered_x(src_line_2, 25)
+        src_x_1 = screen_margin  # get_centered_x(src_line_1, 25)
+        src_x_2 = screen_margin  # get_centered_x(src_line_2, 25)
         draw.text((src_x_1, 105), src_line_1, fill="GREEN", font=Consolas_Bold_25)
         draw.text((src_x_2, 132), src_line_2, fill="GREEN", font=Consolas_25)
 
@@ -144,15 +147,15 @@ try:
         draw.line([(0, 170), (240, 170)], fill = "GRAY", width = 1)
 
         # CPU Temperature ------------------------------------------------------------
-        sys_temp = f"{format(int(read_file('../../../../sys/class/thermal/thermal_zone0/temp'))/1000, ".2f")}°C"
-        sys_fan_rpm = f"{read_file('../../../../sys/class/hwmon/hwmon2/fan1_input')}"
-        sys_temp_x = get_centered_x(" ##:## | ##.#°C | ####", 20)
-        draw.text((sys_temp_x, 175), f"{time_string} | {sys_temp} | {sys_fan_rpm}", fill="GREEN", font=Consolas_Bold_20)
+        # sys_temp = f"{format(int(read_file('../../../../sys/class/thermal/thermal_zone0/temp'))/1000, ".2f")}°C"
+        # sys_fan_rpm = f"{read_file('../../../../sys/class/hwmon/hwmon2/fan1_input')}"
+        # sys_temp_x = get_centered_x(" ##:## | ##.#°C | ####", 20)
+        # draw.text((sys_temp_x, 175), f"{time_string} | {sys_temp} | {sys_fan_rpm}", fill="GREEN", font=Consolas_Bold_20)
 
 
         # NDPi Version & Device IP ---------------------------------------------------
-        line_dev_info = f"(v{ndpi_version}) {device_ip}".strip()
-        line_dev_info_x = get_centered_x(line_dev_info, 20)
+        line_dev_info = f"Version {ndpi_version}".strip()
+        line_dev_info_x = screen_margin  # get_centered_x(line_dev_info, 20)
         draw.text((line_dev_info_x, 195), line_dev_info, fill="GREEN", font=Consolas_Bold_20)
 
         # Device ID ------------------------------------------------------------------
