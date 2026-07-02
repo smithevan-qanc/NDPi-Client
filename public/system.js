861605270a0d139887e50ff0a726159251084006
smithevan-qanc <fwy25569@gmail.com>
2026-07-02 00:41:16 -0500

    [3.1.1218] NOTES:

--- public/system.js
+++ public/system.js
@@ -164,7 +164,7 @@ server._ws.onmessage = (message) => {
 (async () => {
     await refreshSources();
     const savedScrollPos = window.sessionStorage.getItem('scrollPos') || '0';
-    window.scrollTo(0, Number(savedScrollPos));
+    window.scrollTo({ left: 0, top: Number(savedScrollPos), behavior:'smooth' });
     addEvents();
 })();
 
