8caf4b2f94b49868d6a654ce674e8287763cec06
smithevan-qanc <fwy25569@gmail.com>
2026-07-01 22:55:19 -0500

    [3.1.1203] NOTES:

--- index.js
+++ index.js
@@ -200,7 +200,8 @@ class NDPi {
         const name = this.settings.get('device_name');
         const pin = this.settings.get('ndpi_airplay_server_pin') || null;
 
-        let airPlayOptions = ['-n', name, '-nh', '-fs', '-hls', '-fps', '60', '-d' ];
+        let airPlayOptions = ['-n', name, '-nh', '-fs', '-hls', '-fps', '60' ];
+        // let airPlayOptions = ['-n', name, '-nh', '-fs', '-hls', '-fps', '60', '-d' ];
         if (pin && /^\d{1,4}$/.test(pin))  // Validate PIN.
         {
             airPlayOptions.push('-pin');
