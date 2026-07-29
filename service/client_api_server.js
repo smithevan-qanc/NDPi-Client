26196888dfd677b6bcbe56d70115c4833d6c2508
smithevan-qanc <fwy25569@gmail.com>
2026-07-28 19:39:38 -0500

    [3.1.1256] NOTES:

--- service/client_api_server.js
+++ service/client_api_server.js
@@ -436,7 +436,7 @@ class NDPiCommandServer_Client extends EventEmitter {
     }
 
     systemStats() { // /sys/class/thermal/thermal_zone0/temp
-        const thermal_zone0 = fs.readFileSync(path.join('/sys', 'class', 'thermal', 'thermal_zone0', 'temp'), 'utf8');
+        const thermal_zone0 = fs.readFileSync(path.join('/sys', 'class', 'thermal', 'thermal_zone0', 'temp'), 'utf8').trim();
 
         return {
             systemTime:         String(new Date()),
