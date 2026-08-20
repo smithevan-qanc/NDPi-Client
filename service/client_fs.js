3d5b11d0ab3e3c43b821580f937ba925cc51bf4a
smithevan-qanc <fwy25569@gmail.com>
2026-08-20 11:35:10 -0500

    [3.1.1293] NOTES:

--- service/client_fs.js
+++ service/client_fs.js
@@ -421,6 +421,13 @@ class FileSystemMonitor extends EventEmitter {
                 allowEditExternal: false,
             },
             {
+                key: "ndpi_version_update_version_date",
+                value: ``,
+                group: `Backend`,
+                allowEditInternal: true,
+                allowEditExternal: false,
+            },
+            {
                 key: "ndpi_version",
                 value: this.#pgmVersion,
                 group: `Backend`,
